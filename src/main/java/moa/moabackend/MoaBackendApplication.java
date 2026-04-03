d4de2d7e056e17e7d10edc61d8623d3e6d27d181
박지민 <pjmlove2008@gmail.com>
2026-04-03 10:16:52 +0900

    chore: init spring boot project

---
+++ src/main/java/moa/moabackend/MoaBackendApplication.java
@@ -0,0 +1,13 @@
+package moa.moabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaBackendApplication.class, args);
+    }
+
+}
