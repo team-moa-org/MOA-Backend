bb2ed245ab84caddde4e15810df9312b95ca47f7
박지민 <pjmlove2008@gmail.com>
2026-05-25 12:44:05 +0900

    fix: 누락된 임포트 구문 추가

--- src/main/kotlin/moa/moabackend/domain/grouppurchase/domain/repository/GroupPurchaseRepository.kt
+++ src/main/kotlin/moa/moabackend/domain/grouppurchase/domain/repository/GroupPurchaseRepository.kt
@@ -1,5 +1,6 @@
 package moa.moabackend.domain.grouppurchase.domain.repository
 
+import moa.moabackend.domain.grouppurchase.domain.Category
 import moa.moabackend.domain.grouppurchase.domain.GroupPurchase
 import moa.moabackend.domain.grouppurchase.domain.GroupPurchaseStatus
 import org.springframework.data.jpa.repository.JpaRepository
